7095c95a70d2da1ae95b56c4229ec7cf863d2c7e
Jhalak-Gupta <143864523+Jhalak-Gupta@users.noreply.github.com>
2024-01-19 15:13:15 +0530

    Update 2-3.cpp

--- 2-3.cpp
+++ 2-3.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv){
+int main
+{
     int n;
-    
-    
-    //write your code here
     for(int i = 1 ; i<= n ; i++){
-    for(int j = 1 ; j <=n-i ; j++ ){
-        cout<<" ";
-    }
-    for(int j = 1 ; j <= i; j++){
-        cout<<"*";
-    }
-    cout<<endl;
-}
-    
+        for(int j = 1 ; j <=n-i ; j++ ){
+            cout<<" ";
+        }
+        for(int j = 1 ; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }    
 }
