e61376e2ab1780f2efc4e55297a977138b53807e
Jhalak-Gupta <143864523+Jhalak-Gupta@users.noreply.github.com>
2024-01-19 15:19:38 +0530

    Update 2-6.cpp

--- 2-6.cpp
+++ 2-6.cpp
@@ -1,41 +1,28 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main(int argc, char **argv){
     int n;
     cin >> n;
 
-    //write your code here
-    for(int i = 1 ; i<=n-2 ; i++){
-    //star
-    for(int j = 1 ; j <= n-i-1 ; j++){
-        cout<<"*";
+    int stars = n / 2 + 1;
+    int spaces = 1;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= stars; j++){
+            cout << ("*	");
+        }
+        for (int j = 1; j <= spaces; j++){
+            cout << ("	");
+        }
+        for (int j = 1; j <= stars; j++){
+            cout << ("*	");
+        }
+        cout << endl;       
+        if (i < n / 2 + 1){
+            stars--;
+            spaces += 2;
+        }else{
+            stars++;
+            spaces -= 2;
+        }
     }
-    //space
-    for(int j = 1 ; j<= 2*i + 3 - n; j ++ ){
-        cout<<" ";
-    }
-    //star
-    for(int j = 1 ; j<= n-i-1; j++){
-    cout<<"*" ;
-    }  
-cout<<endl;
-}
-for(int i = 1 ; i<=n-3; i++){
-    //star
-    for(int j = 1 ; j<= i+1; j++){
-        cout<<"*";
-    }
-    //space
-    for(int j = 1 ; j<= n - 2*i - 1 ; j ++ ){
-        cout<<" ";
-    }
-    //star
-    for(int j = 1 ; j <= i+1 ; j++){
-    cout<<"*";
-    }  
-cout<<endl;
-}
-
-    
 }
